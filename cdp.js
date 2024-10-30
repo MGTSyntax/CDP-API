c0e8dce97fc5ef464a5b72d30efd04c157c7316b
Marlon Torres <torresmarlon0627@yahoo.com>
2024-10-30 17:42:39 +0800

    updated naming convention and added employees API

--- cdp.js
+++ cdp.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config/config');
 const dbMiddleware = require('./middleware/dbmiddleware');
+const res = require('express/lib/response');
 
 class App {
     constructor() {
@@ -19,10 +20,12 @@ class App {
     }
 
     routes() {
+        // Get Databases
         this.app.get('/databases', (req, res) => {
             res.json(config.databases);
         });
 
+        // Get and Display Departments and Detachments
         this.app.get('/departments', async (req, res) => {
             const dbName = req.query.db;
             if (!dbName) {
@@ -51,11 +54,39 @@ class App {
                 await req.db.close();
             }
         });
+
+        // Get and Display Employees
+        this.app.get('/employees', async (req, res) => {
+            const dbName = req.query.db;
+            if (!dbName) {
+                return res.status(400).json({ error: 'Database name is required.' });
+            }
+            const query = `
+            SELECT
+                a.ji_empNo, a.ji_lname, a.ji_fname, a.ji_mname, a.ji_extname,
+                b.email_add
+            FROM
+                ${dbName}.trans_basicinfo a
+            INNER JOIN
+                ${dbName}.trans_emailadd b ON a.ji_empNo = b.ji_empNo
+            ORDER BY a.ji_lname
+            `;
+
+            try {
+                const results = await req.db.query(query);
+                res.json(results);
+            } catch (err) {
+                res.status(500).json({ error: err.message });
+            } finally {
+                await req.db.close();
+            }
+        });
     }
 
+    // Start the app - node cdp.js
     start() {
         this.app.listen(this.port, () => {
-            console.log(`Server is running on http://localhost:${this.port}/departments`);
+            console.log(`Server is running on http://localhost:${this.port}/`);
         });
     }
 }
